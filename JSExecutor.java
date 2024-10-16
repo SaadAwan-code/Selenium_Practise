d6a8f96ad4e4f6eecc66639d84674a1baf017b2e
Saad abdullah <115576669+SaadAwan-code@users.noreply.github.com>
2024-10-16 15:07:56 +0500

    Add files via upload

---
+++ JSExecutor.java
@@ -0,0 +1,23 @@
+package sampleapp;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JSExecutor {
+    public static void main(String[] args) {
+        System.out.println("javascript automation");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\vadion\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("http://www.google.com");
+        driver.manage().window().maximize();
+        JavascriptExecutor js=(JavascriptExecutor)driver;
+        js.executeScript("prompt('Enter Your Name :')");
+        // driver.quit();
+
+
+
+    }
+}
+
+
